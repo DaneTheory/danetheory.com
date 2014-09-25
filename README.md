1e5c3c185da2cc03a9e98f2e601d363549bcfb1b
Branden Dane <Branden@DaneTheory.com>
2014-09-25 06:31:54 -0400

    DaneTheory.com
    Initial Code Framework Build Push

--- README.md
+++
@@ -1,7 +0,0 @@
-DaneTheory.com
-==============
-
-Landing page for "Coming Soon" website for Branden Dane, web developer and Designer
-
-
-        Here upload coming in hard
--- wp-content/themes/jupiter/sidebar.php
+++
@@ -1,5 +0,0 @@
-<aside id="mk-sidebar" class="mk-builtin">
-    <div class="sidebar-wrapper">
-    <?php  if(isset($post)){theme_class('sidebar',$post->ID);}else{theme_class('sidebar');} ?>
-    </div>
-</aside>
